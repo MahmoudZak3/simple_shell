360cf46d8097b5bfee1715b09c00b18c65eb120d
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-08-19 09:57:54 +0300

    zaaaaaak com

--- aux_lists2.c
+++ aux_lists2.c
@@ -2,7 +2,7 @@
 
 /**
  * add_rvar_node - adds a variable at the end
- * of a r_var list.
+ *
  * @head: head of the linked list.
  * @lvar: length of the variable.
  * @val: value of the variable.
@@ -41,7 +41,7 @@ r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
 /**
  * free_rvar_list - frees a r_var list
  * @head: head of the linked list.
- * Return: no return.
+ * Return:  return.
  */
 void free_rvar_list(r_var **head)
 {
