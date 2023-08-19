52121c21e2fd17727db088a49ac0afbb23ef616e
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-08-19 12:56:04 +0300

    zaaak com

--- split.c
+++ split.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- * swap_char - swaps | and & for non-printed chars
- *
- * @input: input string
+ * swap_char - swaps | and &
+ * @input: input
  * @bool: type of swap
  * Return: swapped string
  */
@@ -44,12 +43,11 @@ char *swap_char(char *input, int bool)
 }
 
 /**
- * add_nodes - add separators and command lines in the lists
- *
- * @head_s: head of separator list
- * @head_l: head of command lines list
- * @input: input string
- * Return: no return
+ * add_nodes - add separators in the lists
+ * @head_s: separator list
+ * @head_l:  command lines list
+ * @input: input
+ * Return:  return 0
  */
 void add_nodes(sep_list **head_s, line_list **head_l, char *input)
 {
@@ -80,8 +78,7 @@ void add_nodes(sep_list **head_s, line_list **head_l, char *input)
 }
 
 /**
- * go_next - go to the next command line stored
- *
+ * go_next - go to the next command
  * @list_s: separator list
  * @list_l: command line list
  * @datash: data structure
@@ -124,9 +121,8 @@ void go_next(sep_list **list_s, line_list **list_l, data_shell *datash)
 /**
  * split_commands - splits command lines according to
  * the separators ;, | and &, and executes them
- *
- * @datash: data structure
- * @input: input string
+ * @datash: data
+ * @input: input
  * Return: 0 to exit, 1 to continue
  */
 int split_commands(data_shell *datash, char *input)
@@ -170,7 +166,6 @@ int split_commands(data_shell *datash, char *input)
 
 /**
  * split_line - tokenizes the input string
- *
  * @input: input string.
  * Return: string splitted.
  */
