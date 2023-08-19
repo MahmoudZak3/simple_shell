1754d7a490025f092392f3042c48fd9e1988f8f9
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-08-19 12:51:59 +0300

    zaaak com

--- read_line.c
+++ read_line.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 
 /**
- * read_line - reads the input string.
- *
- * @i_eof: return value of getline function
- * Return: input string
+ * read_line -  input string.
+ * @i_eof:  value of getline function
+ * Return: input
  */
 char *read_line(int *i_eof)
 {
