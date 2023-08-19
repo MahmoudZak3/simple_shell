18fe067eb7d089a0822dfa3cf567da383de42bf8
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-08-19 10:15:26 +0300

    zaaaaaak com

--- env2.c
+++ env2.c
@@ -2,9 +2,9 @@
 
 /**
  * copy_info - copies info to create
- * a new env or alias
- * @name: name (env or alias)
- * @value: value (env or alias)
+ * a new alias
+ * @name:  env or alias
+ * @value:  env or alias
  *
  * Return: new env or alias.
  */
